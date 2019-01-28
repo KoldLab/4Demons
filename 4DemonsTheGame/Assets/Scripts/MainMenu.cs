ede2b3dd0b011e04f147eea73f5f67ca09a7518e
Sami Steenhaut <s.steenhaut@hotmail.com>
2019-01-28 15:35:05 -0500

    ajout du screen menu

---
+++ 4DemonsTheGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Options()
+    {
+        
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
