6037376c4ed2cdac52c0ec81c9cbfb7583d6624e
Sami Steenhaut <s.steenhaut@hotmail.com>
2019-02-05 15:06:35 -0500

    added camera follow

---
+++ 4DemonsTheGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    private bool doMovement = true;
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //bloquer la camera
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            doMovement = !doMovement;
+        }
+        if (!doMovement)
+            return;
+
+      
+        if (transform.position.x <= 6f && (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness))
+        {
+            transform.Translate(Vector2.right * panSpeed * Time.deltaTime);
+        }
+        if (transform.position.x >= -6 && (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness))
+        {
+            transform.Translate(Vector2.left * panSpeed * Time.deltaTime);
+        }
+        
+
+
+
+    }
+}
