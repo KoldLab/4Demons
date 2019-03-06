bd6653fae4fa9de60c8d326eea9d882b66e480e7
Sami Steenhaut <s.steenhaut@hotmail.com>
2019-03-06 18:31:02 -0500

    added tutorials, animations on button, understood time

--- 4DemonsTheGame/Assets/LevelSelector.cs
+++ 4DemonsTheGame/Assets/LevelSelector.cs
@@ -5,6 +5,10 @@ public class LevelSelector : MonoBehaviour
 {
     public SceneFade fader;
 
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
     public void Select(string levelName)
     {
         fader.FadeTo(levelName);
--- 4DemonsTheGame/Assets/Scripts/SceneFade.cs
+++ 4DemonsTheGame/Assets/Scripts/SceneFade.cs
@@ -8,10 +8,11 @@ public class SceneFade : MonoBehaviour
 {
     public Image img; //image that fades
     public AnimationCurve curve;
+    private Time elapsedTime;
 
     private void Start()
     {
-        StartCoroutine(FadeIn());
+        StartCoroutine(FadeIn());       
     }
 
     public void FadeTo(string scene)
@@ -25,7 +26,7 @@ public class SceneFade : MonoBehaviour
         float t = 1f;
         while (t > 0f)
         {
-            t -= Time.deltaTime * 1f; //plus le chiffre augmente moins le fade est rapide
+            t -= Time.unscaledDeltaTime * 1f; //plus le chiffre augmente moins le fade est rapide
             float a = curve.Evaluate(t); //la fonction va prendre comme x la valeur de T
             img.color = new Color(0f, 0f, 0f, a); // couleur noir qui devient transparente
             yield return 0; //attend la prochaine frame jusqu'a t = 0
@@ -33,12 +34,12 @@ public class SceneFade : MonoBehaviour
 
     }
 
-    IEnumerator FadeOut(string scene) //fonction qui run en background comme un update mais qu'on controle
+    IEnumerator FadeOut(string scene) //fonction  qui run en background comme un update mais qu'on controle
     {
         float t = 0f;
         while (t < 1f)
         {
-            t += Time.deltaTime * 1f; //plus le chiffre augmente moins le fade est rapide
+            t += Time.unscaledDeltaTime * 1f; //plus le chiffre augmente moins le fade est rapide
             float a = curve.Evaluate(t); //la fonction va prendre comme x la valeur de T
             img.color = new Color(0f, 0f, 0f, a); // couleur noir qui devient transparente
             yield return 0; //attend la prochaine frame jusqu'a t = 0
